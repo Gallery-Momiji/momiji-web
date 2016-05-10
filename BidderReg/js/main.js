c68dee33a1e2a11e06193c278574e43e4cb565fb
root <root@Alfred>
2016-05-10 07:24:19 -0400

    Added error/success alerts to index.html Added js logic to hide/show messages depending on GET params Finalized logic on Submit.php

--- BidderReg/js/main.js
+++ BidderReg/js/main.js
@@ -1,1 +1,14 @@
+$.urlParam = function(name) {
+  var results = new RegExp('[\?&amp;]' + name + '=([^&amp;#]*)').exec(window.location.href);
+  if (results === null) return -1;
+  return results[1] || -1;
+}
 
+if ($.urlParam('success') === "0"){
+	$('#success_bidder').hide();
+} else if ($.urlParam('success') === "1"){
+	$('#fail_bidder').hide();
+} else {
+	$('#fail_bidder').hide();
+	$('#success_bidder').hide();
+}
