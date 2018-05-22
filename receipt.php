ac0b83ee485d03b138c8f9702a5888b0c00d93d3
Mystro256 <alexjnewt@hotmail.com>
2018-05-22 12:28:36 -0400

    Silly typo, fix misc payment receipts

--- receipt.php
+++ receipt.php
@@ -214,7 +214,7 @@
 			$temp = getMerchInfo($connection, $identifier['artist'], $identifier['piece']);
 			echo '   <td>' . $temp['MerchTitle'] . "</td>\n";
 		} else {
-			echo '   <td>' . $temp['Misc. Payment'] . "</td>\n";
+			echo '   <td>Misc. Payment</td>\n';
 		}
 		echo '   <td>$' . number_format($prices[$key],2) . "</td></tr>\n";
 	}
