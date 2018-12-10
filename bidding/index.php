3a9baf9bcaeec8c4cb138365dcee49c23133e7be
Mystro256 <alexjnewt@hotmail.com>
2018-12-10 13:49:51 -0500

    Add basic bidding error/success param handling

--- bidding/index.php
+++ bidding/index.php
@@ -1,4 +1,9 @@
 <?php
 	require_once('../config.php');
 	require_once('../inc/mysql.php');
+
+	if (isset($_GET['error']) and $_GET['error'] == "1"){
+		//TODO use better error ouput than plain text
+		echo "Unable to show selected item, please try again.";
+	}
 ?>
--- bidding/item.php
+++ bidding/item.php
@@ -2,6 +2,23 @@
 	require_once('../config.php');
 	require_once('../inc/mysql.php');
 
+	if (isset($_GET['error'])){
+		//TODO use better error ouput than plain text
+		switch ($_GET['error']){
+			case "1":
+				echo "Unable to submit bid, please try again.";
+			case "2":
+				echo "The bidder number you provided was not found. If you have not registered yet, please contact a staff member.";
+			case "3":
+				echo "Oops, looks like you might have been out bid!";
+		}
+	}
+
+	if (isset($_GET['success']) and $_GET['success'] == "1"){
+		//TODO use better output than plain text
+		echo "Your bid has been submitted! Good luck!";
+	}
+
 	if (!isset($_GET['artistid']) or $_GET['artistid'] == "" or !isset($_GET['merchid']) or $_GET['merchid'] == ""){
 		header('Location: index.php?error=1');
 	}
