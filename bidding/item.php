dd35840ece3a3fb9eceea5350f5a1acc060f7ac8
Mystro256 <alexjnewt@hotmail.com>
2019-03-25 15:46:59 -0400

    Add item information to bidding/item.php

--- bidding/item.php
+++ bidding/item.php
@@ -15,6 +15,11 @@
 	}
 	$infoForBidding = $infoForBidding[0];
 	$itemBids = getBidsForMerch($connection, $artistid, $merchid);
+
+	#Commonly used values
+	$MerchMinBid = $infoForBidding['MerchMinBid'];
+	$MerchQuickSale = $infoForBidding['MerchQuickSale'];
+	$MerchSold = $infoForBidding['MerchSold'];
 ?>
 <!doctype html>
 <html class="no-js" lang="">
@@ -91,9 +96,79 @@ echo forceStringLength($artistid,3,0,true).'-'.forceStringLength($merchid,3,0,tr
 	  <div class="alert alert-warning" style="display:none" role="alert" id="warn_live">
         <strong>This piece is going to live auction!</strong> Ask the staff for when the, live auction will happen.
       </div>
+    <div class="container">
+      <form class="form-horizontal">
+
 <?php
-#TODO fill information
+	if ($MerchQuickSale > 0 && count($itemBids) < 1 && $MerchSold != "1"){
+		echo '<div class="form-group">
+<div class="col-sm-2 control-label"><label></label></div>
+<div class="col-sm-2 control-label">This piece has no bids and can be bought outright for <strong>$'.$MerchQuickSale.'</strong>. See a staff member for details.</div>
+</div>';
+	}
 ?>
+	<div class="form-group">
+          <div class="col-sm-2 control-label"><label>Name of Piece:</label></div>
+          <div class="col-sm-2 control-label"><?php echo $infoForBidding['MerchTitle'];?>
+          </div>
+        </div>
+<?php
+	if ($MerchMinBid < $MerchQuickSale){
+		echo '<div class="form-group">
+<div class="col-sm-2 control-label"><label><strong>Minimum Bid ($):</strong></label></div>
+<div class="col-sm-2 control-label"><label>'.$MerchMinBid.'</label></div>
+</div>';
+	}
+?>
+	<div class="form-group">
+          <div class="col-sm-2 control-label"><label>Medium of the Piece:</label></div>
+          <div class="col-sm-2 control-label"><?php echo $infoForBidding['MerchMedium'];?>
+          </div>
+        </div>
+      </form>
+    </div>
+
+  <form class="form-horizontal" action="submit.php?artistid=<?php
+echo $artistid.'&merchid='.$merchid;
+?>" method="post" id="bidForm" style="display:none">
+    <fieldset>
+
+    <!-- Bid Number input-->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="bnumber">Bidder Number<br></label>
+      <div class="col-md-2">
+      <input autocomplete="off" id="bnumber" name="bnumber" type="number" class="form-control input-md" required="">
+      </div>
+    </div>
+
+    <!-- Name input-->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="name">Name</label>
+      <div class="col-md-4">
+      <input autocomplete="off" id="name" name="name" type="text" placeholder="First and Last" minlength=2 class="form-control input-md" required="">
+      </div>
+    </div>
+
+    <!-- Bid Number input-->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="bamount">Bid amount ($)<br></label>
+      <div class="col-md-2">
+      <input autocomplete="off" id="bamount" name="bamount" type="number" placeholder="<?php
+echo $MerchMinBid;
+?> or more" class="form-control input-md" required="">
+      </div>
+    </div>
+
+    <!-- Submit Button -->
+    <div class="form-group">
+      <label class="col-md-4 control-label" for="submit"></label>
+      <div class="col-md-4">
+      <button id="submit" name="submit" class="btn btn-primary">Submit Bid</button>
+      </div>
+    </div>
+
+    </fieldset>
+  </form>
 	  </div>
 
       <hr>
@@ -110,15 +185,14 @@ echo forceStringLength($artistid,3,0,true).'-'.forceStringLength($merchid,3,0,tr
 		<script src="../inc/js/urlParam.js"></script>
         <script src="js/item.js"></script>
 <script><?php
-	if ($infoForBidding['MerchSold'] == "1"){
+	if ($MerchSold == "1"){
 		echo "$('#warn_sold').show();";
 	} else if ($infoForBidding['AuctionEnd'] == "1"){
 		echo "$('#warn_over').show();";
 	} else if (count( $itemBids ) >= $infoForBidding['AuctionCutoff']){
 		echo "$('#warn_live').show();";
-	} else {
-		#TODO Remove comment once button is added
-		#echo "$('#submit').prop("disabled",!this.checked);";
+	} else if ($MerchMinBid < $MerchQuickSale){
+		echo "$('#bidForm').show();";
 	}
 ?></script>
     </body>
