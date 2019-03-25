c805c9c817b405aa349f9ed445d6f1569596baed
Mystro256 <alexjnewt@hotmail.com>
2019-03-25 13:05:43 -0400

    Add html and js to bidding/item.php

--- bidding/item.php
+++ bidding/item.php
@@ -1,23 +1,7 @@
 <?php
 	require_once('../config.php');
 	require_once('../inc/mysql.php');
-
-	if (isset($_GET['error'])){
-		//TODO use better error ouput than plain text
-		switch ($_GET['error']){
-			case "1":
-				echo "Unable to submit bid, please try again.";
-			case "2":
-				echo "The bidder number you provided was not found. If you have not registered yet, please contact a staff member.";
-			case "3":
-				echo "Oops, looks like you might have been out bid!";
-		}
-	}
-
-	if (isset($_GET['success']) and $_GET['success'] == "1"){
-		//TODO use better output than plain text
-		echo "Your bid has been submitted! Good luck!";
-	}
+	require_once('../inc/util.php');
 
 	if (!isset($_GET['artistid']) or $_GET['artistid'] == "" or !isset($_GET['merchid']) or $_GET['merchid'] == ""){
 		header('Location: index.php?error=1');
@@ -30,20 +14,112 @@
 		header('Location: index.php?error=1');
 	}
 	$infoForBidding = $infoForBidding[0];
-
-	if ($infoForBidding['AuctionEnd'] == "1"){
-		//TODO use better output than plain text
-		echo "Sorry! The auction is now closed!";
-	}
-
-	if ($infoForBidding['MerchSold'] == "1"){
-		//TODO use better output than plain text
-		echo "This piece has already been sold!";
-	} else {
-		$itemBids = getBidsForMerch($connection, $artistid, $merchid);
-		if (count( $itemBids ) >= $infoForBidding['AuctionCutoff']){
-			//TODO use better output than plain text
-			echo "This piece is going to live auction! Ask the staff for when the, live auction will happen.";
-		}
-	}
+	$itemBids = getBidsForMerch($connection, $artistid, $merchid);
 ?>
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="../inc/css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 50px;
+                padding-bottom: 20px;
+            }
+        </style>
+        <link rel="stylesheet" href="../inc/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="../inc/css/main.css">
+
+        <!--[if lt IE 9]>
+            <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            <script>window.html5 || document.write('<script src="../inc/js/vendor/html5shiv.js"><\/script>')</script>
+        <![endif]-->
+    </head>
+    <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Gallery Momiji - Silent Auction Bidding</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <!-- Main jumbotron -->
+    <div class="jumbotron">
+      <div class="container">
+        <p id="bannermessage">AN<?php
+echo forceStringLength($artistid,3,0,true).'-'.forceStringLength($merchid,3,0,true);
+?></p>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+      <!-- Alert Messages -->
+	  <div class="alert alert-success" style="display:none" role="alert" id="success_bid">
+        <strong>Your bid has been submitted!</strong> Good luck!
+      </div>
+	  <div class="alert alert-danger" style="display:none" role="alert" id="fail_error1">
+        <strong>Error:</strong> Unable to submit bid, please try again.
+      </div>
+	  <div class="alert alert-danger" style="display:none" role="alert" id="fail_error2">
+        <strong>Error:</strong> The bidder number you provided was not found. If you have not registered yet, please contact a staff member.
+      </div>
+	  <div class="alert alert-danger" style="display:none" role="alert" id="fail_error3">
+        <strong>Oops,</strong> looks like you might have been out bid!
+      </div>
+	  <div class="alert alert-warning" style="display:none" role="alert" id="warn_over">
+        <strong>Sorry!</strong> The auction is now closed!
+      </div>
+	  <div class="alert alert-warning" style="display:none" role="alert" id="warn_sold">
+        <strong>Sorry!</strong> This piece has already been sold!
+      </div>
+	  <div class="alert alert-warning" style="display:none" role="alert" id="warn_live">
+        <strong>This piece is going to live auction!</strong> Ask the staff for when the, live auction will happen.
+      </div>
+<?php
+#TODO fill information
+?>
+	  </div>
+
+      <hr>
+
+      <!-- Foot Note -->
+      <footer>
+        <p>&copy; 2019 Anime North, Gallery Momiji</p>
+        <p>This is open source! Find the source code on <a href=https://github.com/Gallery-Momiji>GitHub!</a></p>
+      </footer>
+    </div> <!-- Scripts -->
+        <script>window.jQuery || document.write('<script src="../inc/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="../inc/js/vendor/bootstrap.min.js"></script>
+		<script src="../inc/js/urlParam.js"></script>
+        <script src="js/item.js"></script>
+<script><?php
+	if ($infoForBidding['MerchSold'] == "1"){
+		echo "$('#warn_sold').show();";
+	} else if ($infoForBidding['AuctionEnd'] == "1"){
+		echo "$('#warn_over').show();";
+	} else if (count( $itemBids ) >= $infoForBidding['AuctionCutoff']){
+		echo "$('#warn_live').show();";
+	} else {
+		#TODO Remove comment once button is added
+		#echo "$('#submit').prop("disabled",!this.checked);";
+	}
+?></script>
+    </body>
+</html>
---
+++ bidding/js/item.js
@@ -0,0 +1,15 @@
+switch($.urlParam('error')) {
+  case "1":
+    $('#fail_error1').show();
+    break;
+  case "2":
+    $('#fail_error2').show();
+    break;
+  case "3":
+    $('#fail_error3').show();
+    break;
+  default:
+    if ($.urlParam('success') !== false){
+      $('#success_bid').show();
+    }
+}
