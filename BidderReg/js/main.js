31372e6ca676fa9879f4593ccfeea9e118276248
Mystro256 <alexjnewt@hotmail.com>
2022-07-11 22:19:17 -0400

    Add consent of information confirmation

--- BidderReg/js/main.js
+++ BidderReg/js/main.js
@@ -7,14 +7,16 @@ if ($.urlParam('error') !== false){
 *	whithin the dom to re-use instead of constantly re-querying them!
 */
 var termsCheckBox = $('#terms');
+var terms2CheckBox = $('#terms2');
 var submitBtn = $('#submit');
 /**
 *	This is an event listenner applied to the checkbox for terms and agreement.
 *	For each click, a checked state is evaluated and applied as a disable property
 *	to the submit button!
 */
-termsCheckBox.click( 
-	function(){
-		submitBtn.prop( "disabled", !this.checked );
-	}
-);
+function updateSubmitBtn(){
+	submitBtn.prop( "disabled", !document.getElementById("terms").checked || !document.getElementById("terms2").checked );
+}
+termsCheckBox.click(function(){updateSubmitBtn()});
+terms2CheckBox.click(function(){updateSubmitBtn()});
+updateSubmitBtn();
